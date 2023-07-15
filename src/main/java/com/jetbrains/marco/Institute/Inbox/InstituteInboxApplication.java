193ef798c09cc32565bf14688ab95df3286fcac0
garvit <agarvit1142000@gmail.com>
2023-07-15 23:36:26 +0530

    InsituteInboxJava

---
+++ src/main/java/com/jetbrains/marco/Institute/Inbox/InstituteInboxApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.marco.Institute.Inbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstituteInboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstituteInboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetbrains/marco/Institute/Inbox/InstituteInboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.marco.Institute.Inbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstituteInboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
