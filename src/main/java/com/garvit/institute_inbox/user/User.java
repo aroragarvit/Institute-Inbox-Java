0b1de2c06f90821225c296fd612cee9615bb65f0
Garvit <agarvit1142000@gmail.com>
2023-07-23 13:47:15 +0530

    refactored user

--- src/main/java/com/garvit/institute_inbox/user/User.java
+++ src/main/java/com/garvit/institute_inbox/user/User.java
@@ -4,36 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private long id;
     private String name;
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String gender;
+    private String hostel;
+    private String block;
+    private boolean isAvailable;
 }
